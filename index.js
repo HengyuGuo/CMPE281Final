52fc1a359106a9b1b37e2e7ea831816da5210e44
hengyuguo <hengyu.guo@yahoo.com>
2016-04-09 09:35:10 -0700

    change setting

--- index.js
+++ index.js
@@ -10,12 +10,20 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+// app.get('/', function(request, response) {
+//   response.render('pages/index');
+// });
 
 app.get('/cool', function(request, response) {
   response.send(cool());
+});
+
+app.get('/', function(request, response) {
+  var result = ''
+  var times = process.env.TIMES || 5
+  for (i=0; i < times; i++)
+    result += cool();
+  response.send(result);
 });
 
 app.listen(app.get('port'), function() {
